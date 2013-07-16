45500e545c066cd1730dcba06b4c6be67947fd37
Albert Toledo <toledoalbert@gmail.com>
2013-07-16 15:43:48 -0400

    variable class added

--- src/org/tekkotsu/api/Behavior.java
+++ src/org/tekkotsu/api/Behavior.java
@@ -1,5 +1,11 @@
 package org.tekkotsu.api;
 
 public class Behavior {
-
+	
+	//Attributes
+	private String name;
+	private NodeClass nodeClass;
+	
+	//
+	
 }
---
+++ src/org/tekkotsu/api/Variable.java
@@ -0,0 +1,33 @@
+package org.tekkotsu.api;
+
+public class Variable {
+
+	//Attributes
+	private String type;
+	private String value;
+	
+	//Constructor
+	public Variable(String type, String value){
+		this.type = type;
+		this.value = value;
+	}
+	
+	//Mutators
+	public void setType(String type){
+		this.type = type;
+	}
+	
+	public void setValue(String value){
+		this.value = value;
+	}
+	
+	//Accessor
+	public String getType(){
+		return type;
+	}
+	
+	public String getValue(){
+		return value;
+	}
+	
+}
