9178946a87d9fe08a1796824a1ecfa7f25160d46
toledoalbert <toledoalbert@gmail.com>
2013-07-22 18:29:18 -0400

    Mapbuilder wizard stuff improved and some names changed

---
+++ src/org/tekkotsu/api/MapBuilderWizard.java
@@ -0,0 +1,74 @@
+package org.tekkotsu.api;
+
+import java.util.ArrayList;
+
+public class MapBuilderWizard {
+
+	//Attributes
+	private String name;
+	private String map;
+	private boolean aprilTag;
+	private ArrayList<MapRequestObject> objects = new ArrayList<MapRequestObject>();
+	
+	//Constructor
+	public MapBuilderWizard(String name, String map, boolean april){
+		this.name = name;
+		this.map = map;
+		this.aprilTag = april;
+		this.objects = new ArrayList<MapRequestObject>();
+	}
+	
+	public MapBuilderWizard(){
+		this.name = "";
+		this.map = "";
+		this.aprilTag = true;
+		this.objects = new ArrayList<MapRequestObject>();
+	}
+	
+	//Accessor methods
+	public String getName(){
+		return name;
+	}
+	
+	public String getMap(){
+		return map;
+	}
+	
+	public boolean getAprilTag(){
+		return aprilTag;
+	}
+	
+	public ArrayList<MapRequestObject> getObjects(){
+		return objects;
+	}
+	
+	
+	//Mutators
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setMap(String map){
+		this.map = map;
+	}
+	
+	public void setAprilTag(boolean april){
+		this.aprilTag = april;
+	}
+	
+	
+	//Adding and removing requests
+	public void addRequestObject(MapRequestObject obj){
+		this.objects.add(obj);
+	}
+	
+	public void removeRequestObject(MapRequestObject obj){
+		this.objects.remove(obj);
+	}
+	
+	public void removeRequestObject(int i){
+		this.objects.remove(i);
+	}
+	
+	
+}
--- src/org/tekkotsu/api/MapBuilderWriter.java
+++ src/org/tekkotsu/api/MapBuilderWriter.java
@@ -2,4 +2,35 @@ package org.tekkotsu.api;
 
 public class MapBuilderWriter {
 
+	//Attributes
+	private MapBuilderWizard wizard;
+	private Method doStart;
+	private NodeClass nClass;
+	
+	//Constructor
+	public MapBuilderWriter(MapBuilderWizard wizard){
+		this.wizard = wizard;
+		this.doStart = new Method("doStart", "void");
+		this.nClass = new NodeClass("LookForObjects", new ConstructorCall("const"));
+	}
+	
+	//
+	public String getCode(){
+		
+		//Header string
+		String code = "";
+		
+		//Return the code string.
+		return code;
+		
+	}
+	
+	public Method getDoStart(){
+		
+		//add the code body
+		doStart.setBody(getCode());
+		
+		//Return the method dostart
+		return doStart;
+	}
 }
