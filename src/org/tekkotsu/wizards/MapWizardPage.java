60ca2f80bce0cb2e7bacebc5c663404bbe2b5f48
toledoalbert <toledoalbert@gmail.com>
2013-07-26 22:35:26 -0400

    wizardpage done

--- src/org/tekkotsu/wizards/MapWizardPage.java
+++ src/org/tekkotsu/wizards/MapWizardPage.java
@@ -14,11 +14,10 @@ import org.tekkotsu.api.MapBuilderWizard;
 import org.tekkotsu.api.MapBuilderWriter;
 
 public class MapWizardPage extends WizardPage {
-  private Text name;
-  private Text map;
-  private Button april;
-  private Button pursueShapes;
-  private String code;
+  private Text name, map;
+  private Button april, pursueShapes;
+  private Label lName, lMap, lApril, lObjects, lRed, lGreen, lBlue, lLine, lCylinder, lBlob, lPursueShapes;
+  private Button lineR, lineG, lineB, cylR, cylG, cylB, blobR, blobB, blobG;
   private MapBuilderWizard wiz;
   private Composite container;
 
@@ -35,27 +34,91 @@ public class MapWizardPage extends WizardPage {
     container = new Composite(parent, SWT.NONE);
     GridLayout layout = new GridLayout();
     container.setLayout(layout);
-    layout.numColumns = 2;
+    layout.numColumns = 4;
+    layout.makeColumnsEqualWidth = true;
     
-    //Name
-    Label nameLabel = new Label(container, SWT.NONE);
-    nameLabel.setText("Name");
-    name = new Text(container, SWT.BORDER | SWT.SINGLE);
+
+    //Row one for name
+    lName = new Label(container, SWT.NONE);
+    lName.setText("Name");
+    
+    name = new Text(container, SWT.SINGLE | SWT.BORDER);
     name.setText("");
+    GridData nameGrid = new GridData(); 
+    nameGrid.horizontalSpan = 3;  			
+    nameGrid.horizontalAlignment = SWT.FILL;	//TODO doesnt fucking work
+    name.setLayoutData(nameGrid);
     
-    //Map
-    Label mapLabel = new Label(container, SWT.NONE);
-    mapLabel.setText("Map");
-    map = new Text(container, SWT.BORDER | SWT.SINGLE);
+    //Row two for map
+    lMap = new Label(container, SWT.NONE);
+    lMap.setText("Map");
+    
+    map = new Text(container, SWT.SINGLE | SWT.BORDER);
     map.setText("");
+    GridData mapGrid = new GridData(); 
+    mapGrid.horizontalSpan = 3;
+    mapGrid.horizontalAlignment = SWT.FILL;	//TODO doesnt fucking work
+    map.setLayoutData(mapGrid);
     
-    //AprilTag
-    Label aprilLabel = new Label(container, SWT.NONE);
-    aprilLabel.setText("AprilTag");
-    april = new Button(container, SWT.RADIO);
+    //Row three for april tag
+    lApril = new Label(container, SWT.NONE);
+    lApril.setText("April Tag");
+    
+    april = new Button(container, SWT.TOGGLE);
+    GridData aprilGrid = new GridData(); 
+    aprilGrid.horizontalSpan = 3;
+    april.setLayoutData(aprilGrid);
+    
+    /*
+    //Composite for the object selections
+    Composite objectGroup = new Composite(container, SWT.NONE);
+    GridLayout objLayout = new GridLayout();
+    objLayout.numColumns = 4;
+    objLayout.makeColumnsEqualWidth = true;
+    objectGroup.setLayout(objLayout);
+    */
+    
+    //Row four for Objects and color names
+    lObjects = new Label(container, SWT.NONE); 
+    lRed = new Label(container, SWT.NONE); 
+    lGreen = new Label(container, SWT.NONE); 
+    lBlue = new Label(container, SWT.NONE);
+    
+    lObjects.setText("Objects");
+    lRed.setText("Red");
+    lGreen.setText("Green");
+    lBlue.setText("Blue");
+    
+    //Row five for line object
+    lLine = new Label(container, SWT.NONE); 
+    lLine.setText("Line");
+    lineR = new Button(container, SWT.TOGGLE);
+    lineG = new Button(container, SWT.TOGGLE);
+    lineB = new Button(container, SWT.TOGGLE);
+    
+    //Row six for cylinder object
+    lCylinder = new Label(container, SWT.NONE); 
+    lCylinder.setText("Cylinder");
+    cylR = new Button(container, SWT.TOGGLE);
+    cylG = new Button(container, SWT.TOGGLE);
+    cylB = new Button(container, SWT.TOGGLE);
+    
+    //Row seven for blob object
+    lBlob = new Label(container, SWT.NONE); 
+    lBlob.setText("Blob");
+    blobR = new Button(container, SWT.TOGGLE);
+    blobG = new Button(container, SWT.TOGGLE);
+    blobB = new Button(container, SWT.TOGGLE);
+    
+    //Row eight for pursueShapes
+    lPursueShapes = new Label(container, SWT.FILL);
+    lPursueShapes.setText("PursueShapes");
+    
+    pursueShapes = new Button(container, SWT.TOGGLE);
     
     
     
+
     name.addKeyListener(new KeyListener() {
 
       @Override
@@ -71,8 +134,10 @@ public class MapWizardPage extends WizardPage {
       }
 
     });
-    GridData gd = new GridData(GridData.FILL_HORIZONTAL);
-    name.setLayoutData(gd);
+    
+    
+    
+    
     // Required to avoid an error in the system
     setControl(container);
     setPageComplete(false);
